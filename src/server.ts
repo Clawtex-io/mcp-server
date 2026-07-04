05f26eb4b735993df45715fd354a6c05f37930b2
Paul Evans <paul@evans-studio.co.uk>
2026-07-04 14:46:21 +0100

    0.2.0: login command (device-auth one-command setup)

--- src/server.ts
+++ src/server.ts
@@ -39,7 +39,7 @@ if (!apiKey) {
 const client = new ClawtexClient(apiKey);
 
 const server = new Server(
-  { name: "clawtex", version: "0.1.3" },
+  { name: "clawtex", version: "0.2.0" },
   { capabilities: { tools: {}, resources: {} } }
 );
 
